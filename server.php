3af76abb4b29c7c6a1aa6d477fafaa961ebebdf9
diona_coder1 <diona_coder1@example.com>
2026-04-10 18:20:57 +0200

    added UDP socket and bind

--- server.php
+++ server.php
@@ -3,6 +3,12 @@
 $ip = "0.0.0.0";
 $port = 5000;
 
-echo "Serveri startoi në $ip:$port\n";
+// krijo socket UDP
+$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+// lidh socket me IP dhe port
+socket_bind($socket, $ip, $port);
+
+echo "Serveri është duke dëgjuar në $ip:$port...\n";
 
 ?>
