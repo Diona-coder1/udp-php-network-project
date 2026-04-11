8d5d25a6f7fc8cc86d4673a1b5b53cf8bddf3e79
diona_coder1 <diona_coder1@example.com>
2026-04-11 22:06:47 +0200

    added client tracking and message storage

--- server.php
+++ server.php
@@ -3,12 +3,22 @@
 $ip = "0.0.0.0";
 $port = 5000;
 
-// krijo socket UDP
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-
-// lidh socket me IP dhe port
 socket_bind($socket, $ip, $port);
 
 echo "Serveri është duke dëgjuar në $ip:$port...\n";
+
+while (true) {
+    $buf = '';
+    $client_ip = '';
+    $client_port = 0;
+
+    socket_recvfrom($socket, $buf, 1024, 0, $client_ip, $client_port);
+
+    echo "Mesazh nga $client_ip:$client_port -> $buf\n";
+
+    $response = "Mesazhi u pranua";
+    socket_sendto($socket, $response, strlen($response), 0, $client_ip, $client_port);
+}
 
 ?>
