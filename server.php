8a0cf1fc72d8147ab94436dcb3ba4be15a2a8703
diona_coder1 <diona_coder1@example.com>
2026-04-12 16:42:00 +0200

    added client timeout handling

--- server.php
+++ server.php
@@ -8,6 +8,9 @@ socket_bind($socket, $ip, $port);
 
 echo "Serveri është duke dëgjuar në $ip:$port...\n";
 
+$clients = [];
+$messages = [];
+
 while (true) {
     $buf = '';
     $client_ip = '';
@@ -15,10 +18,22 @@ while (true) {
 
     socket_recvfrom($socket, $buf, 1024, 0, $client_ip, $client_port);
 
+    $client_key = "$client_ip:$client_port";
+    $clients[$client_key] = time();
+
+    $messages[] = "$client_key -> $buf";
+
     echo "Mesazh nga $client_ip:$client_port -> $buf\n";
 
     $response = "Mesazhi u pranua";
     socket_sendto($socket, $response, strlen($response), 0, $client_ip, $client_port);
+    
+    foreach ($clients as $client => $last_time) {
+    if (time() - $last_time > 30) {
+        unset($clients[$client]);
+        echo "Klienti $client u largua (timeout)\n";
+    }
+}
 }
 
 ?>
