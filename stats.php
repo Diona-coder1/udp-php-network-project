917a81c8791e9feb8dd221a09532b55cf9aa8981
nati-hs <donat.hasani@student.uni-pr.edu>
2026-04-13 21:37:22 +0200

    Added function to read log files

--- stats.php
+++ stats.php
@@ -1,3 +1,9 @@
 <?php
 header('Content-Type: application/json');
+
+function read_file($file) {
+    if (!file_exists($file)) return [];
+    return file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+}
+
 ?>
