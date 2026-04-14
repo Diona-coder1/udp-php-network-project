e85783ca0d4e5c91d7d2056029b6ce5428c959cd
nati-hs <donat.hasani@student.uni-pr.edu>
2026-04-14 19:52:04 +0200

    Generate JSON output with active clients, IPs, and message data

--- stats.php
+++ stats.php
@@ -8,4 +8,16 @@ function read_file($file) {
 
 $clients = read_file("clients.log");
 $messages = read_file("messages.log");
+
+$unique_ips = array_values(array_unique($clients));
+
+$response = [
+    "status" => "OK",
+    "active_clients" => count($unique_ips),
+    "ip_addresses" => $unique_ips,
+    "messages_count" => count($messages),
+    "messages" => array_slice($messages, -50)
+];
+
+echo json_encode($response, JSON_PRETTY_PRINT);
 ?>
