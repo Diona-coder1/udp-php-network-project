83ee62ad244ab9392d71a240923c0ce0ac1663bb
nati-hs <donat.hasani@student.uni-pr.edu>
2026-04-14 15:27:28 +0200

    Getting data from logs

--- stats.php
+++ stats.php
@@ -6,4 +6,6 @@ function read_file($file) {
     return file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 }
 
+$clients = read_file("clients.log");
+$messages = read_file("messages.log");
 ?>
