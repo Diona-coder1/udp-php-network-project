7dc39fd87782bea2dc8771b228abb8b872026e67
nati-hs <donat.hasani@student.uni-pr.edu>
2026-04-15 19:57:10 +0200

    fixed my function and its structure,added secure file paths

--- stats.php
+++ stats.php
@@ -1,13 +1,18 @@
 <?php
 header('Content-Type: application/json');
 
-function read_file($file) {
-    if (!file_exists($file)) return [];
+function read_file_safe($file) {
+    if (!file_exists($file)) {
+        return [];
+    }
     return file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 }
 
-$clients = read_file("clients.log");
-$messages = read_file("messages.log");
+$clients_file = __DIR__ . "/clients.log";
+$messages_file = __DIR__ . "/messages.log";
+
+$clients = read_file_safe($clients_file);
+$messages = read_file_safe($messages_file);
 
 $unique_ips = array_values(array_unique($clients));
 
