9b375faefb32c60105ea4f2b5128632a9266ea12
Agnesa145 <agnesa.godeni@student.uni-pr.edu>
2026-04-17 21:26:31 +0200

    Update client.php

--- client.php
+++ client.php
@@ -5,38 +5,18 @@ $serverPort = 5000;
 
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 
-if (!$socket) {
-    die("Failed to create socket\n");
-}
-
-echo "Username (admin/user): ";
+echo "Enter username: ";
 $user = trim(fgets(STDIN));
 
-echo "Connected to server...\n";
-
-if ($user === "admin") {
-    echo "ADMIN MODE\n";
-    echo "Commands:\n";
-    echo "/list\n";
-    echo "/info\n";
-    echo "/read filename\n";
-    echo "/upload filename|content\n";
-    echo "/delete filename\n";
-    echo "/search keyword\n\n";
-} else {
-    echo "USER MODE (only chat)\n\n";
-}
+echo "Type 'login' to start system\n\n";
 
 while (true) {
 
     echo ">> ";
     $msg = trim(fgets(STDIN));
 
-    if ($msg === "exit") {
-        break;
-    }
+    if ($msg === "exit") break;
 
-    // format: user|message
     $packet = $user . "|" . $msg;
 
     socket_sendto(
@@ -51,11 +31,8 @@ while (true) {
     $from = "";
     $port = 0;
 
-    socket_recvfrom($socket, $response, 2048, 0, $from, $port);
+    socket_recvfrom($socket, $response, 4096, 0, $from, $port);
 
     echo "SERVER: $response\n";
 }
-
-socket_close($socket);
-
 ?>
