a2c683ef83f3b4f6f46dbd8f1603a92621fccca4
Agnesa145 <agnesa.godeni@student.uni-pr.edu>
2026-04-19 19:23:37 +0200

    Update test_client.php

--- test_client.php
+++ test_client.php
@@ -3,7 +3,7 @@
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 
 $message = "Hello Server";
-socket_sendto($socket, $message, strlen($message), 0, "127.0.0.1", 5000);
+socket_sendto($socket, $message, strlen($message), 0, "192.168.178.44", 5000);
 
 echo "Mesazhi u dergua\n";
 
